f2114623e17633a73e0be043266e2f07364039aa
Nguyen Anh Duc <ducna30@ghtk.co>
2023-09-13 01:21:25 +0700

    Update model

--- src/model/model.py
+++ src/model/model.py
@@ -1,13 +1,11 @@
 import torch
 from torch import nn
-from config.model_config import EncoderConfig, ModelConfig, ClassifierConfig, NERClassifierConfig
-from allennlp.modules.elmo import Elmo
-from utils.constansts import ELMO_OPTIONS_FILE, ELMO_WEIGHT_FILE
-from model.GNN import GNN
+from .utils import process_long_input
 
 
 class ATLOPGCN(nn.Module):
-    def __init__(self, config, bert_model, emb_size=768, block_size=64, num_labels=2):
+    def __init__(self, config, bert_model, emb_size=768, block_size=64, num_labels=2,
+                 node_type_embedding=50):
         super().__init__()
         self.config = config
         self.bert_model = bert_model
@@ -20,6 +18,59 @@ class ATLOPGCN(nn.Module):
         self.emb_size = emb_size
         self.block_size = block_size
         self.num_labels = num_labels
+        self.offset = 1
+        self.node_type_embedding = nn.Embedding(3, node_type_embedding)
 
+    def encode(self, input_ids, attention_mask):
+        config = self.config
+        if config.transformer_type == "bert":
+            start_tokens = [config.cls_token_id]
+            end_tokens = [config.sep_token_id]
+        elif config.transformer_type == "roberta":
+            start_tokens = [config.cls_token_id]
+            end_tokens = [config.sep_token_id, config.sep_token_id]
+        else:
+            raise NotImplementedError()
+        sequence_output, attention = process_long_input(self.bert_model, input_ids, attention_mask, start_tokens, end_tokens)
+        return sequence_output, attention
 
-    def forward(self, input_ids, attention_mask):
+    def get_sent_embed(self, sequence_output, batch_sent_pos, num_sent):
+        batch_size, _, embed_dim = sequence_output.shape
+        sent_embed = torch.zeros((batch_size, num_sent, embed_dim))
+        for batch_id, sent_pos in enumerate(batch_sent_pos):
+            for sent_id, pos in enumerate(sent_pos):
+                sent_embed[batch_id, sent_id] = sequence_output[batch_id, pos[0] + self.offset]
+        return sent_embed
+
+    def get_mention_embed(self, sequence_output, batch_entity_pos, num_mention):
+        batch_size, _, embed_dim = sequence_output.shape
+        mention_embed = torch.zeros((batch_size, num_mention, embed_dim))
+        for batch_id, entity_pos in enumerate(batch_entity_pos):
+            mention_id = 0
+            for ent_pos in entity_pos:
+                for mention_pos in ent_pos:
+                    mention_embed[batch_id, mention_id] = sequence_output[batch_id, mention_pos[0] + self.offset]
+                    mention_id += 1
+        return mention_embed
+
+    def get_entity_embed(self, sequence_output, batch_entity_pos, num_entity):
+        batch_size, _, embed_dim = sequence_output.shape
+        entity_embed = torch.zeros((batch_size, num_entity, embed_dim))
+        for batch_id, entity_pos in enumerate(batch_entity_pos):
+            for entity_id, ent_pos in enumerate(entity_pos):
+                embeds = []
+                for mention_pos in ent_pos:
+                    embeds.append(sequence_output[batch_id, mention_pos[0] + self.offset])
+                entity_embed[batch_id, entity_id] = torch.logsumexp(torch.stack(embeds, dim=0), dim=0)
+        return entity_embed
+
+    def forward(self, input_ids, attention_mask,
+                entity_pos, sent_pos,
+                graph, num_mention, num_entity, num_sent,
+                labels, hts):
+        sequence_output, attention = self.encode(input_ids, attention_mask)
+        sent_embed = self.get_sent_embed(sequence_output, sent_pos, num_sent)
+        mention_embed = self.get_mention_embed(sequence_output, entity_pos, num_mention)
+        entity_embed = self.get_entity_embed(sequence_output, entity_pos, num_entity)
+
+        return None
